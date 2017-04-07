d2811039acaf03a19a9bcfc2ca1e9ecec2cdd709
Marco Dalla Stella <marco@7bridges.eu>
2017-04-08 00:43:26 +0200

    First massive configuration.

--- init.el
+++ init.el
@@ -9,19 +9,11 @@
 ;; This file is not part of GNU Emacs.
 
 ;;; Code:
-(setq stack-trace-on-error t)
 
-;;; Tune up garbage collector
-(setq gc-cons-threshold most-positive-fixnum)
+;; Increase the garbage collection threshold to 500 MB to ease startup
+(setq gc-cons-threshold (* 500 1024 1024))
 
-;; Reset threshold to its default after Emacs has startup, because a large
-;; GC threshold equates to longer delays whenever GC happens
-(defun my-set-gc-threshold ()
-  "Reset gc-cons-threshold to its default value"
-  (setq gc-cons-threshold 800000))
-(add-hook 'emacs-startup-hook 'my-set-gc-threshold)
-
-;;; Package setup
+;; List package archives and initialize them
 (require 'package)
 (setq package-enable-at-startup nil)
 (setq package-archives
@@ -30,49 +22,28 @@
         ("MELPA"    . "https://melpa.org/packages/")))
 (package-initialize)
 
-;; Always load newer compiled files
-(setq load-prefer-newer t)
+(setq load-prefer-newer t)              ; Always load newer compiled files
+(setq ad-redefinition-action 'accept)   ; Silence advice redefinition warnings
+(setq message-log-max 10000)            ; Debugging
 
-;; Silence advice redefinition warnings
-(setq ad-redefinition-action 'accept)
-
-;; Debuggingq
-(setq message-log-max 10000)
-
-;; Bootstrap `use-package' and `dash'
-(unless (and (package-installed-p 'use-package)
-             (package-installed-p 'dash))
+;; Make sure Org is installed
+(unless (package-installed-p 'org)
   (package-refresh-contents)
-  (package-install 'use-package)
-  (package-install 'dash))
+  (package-install 'org))
 
-(require 'use-package)
-(require 'dash)
+;; Org plus contrib needs to be loaded before any org related functionality is called
+(unless (package-installed-p 'org-plus-contrib)
+  (package-refresh-contents)
+  (package-install 'org-plus-contrib))
 
-;;; Validation
-(use-package validate                   ; Validate options
-  :ensure t)
+;; Load config.org - my Emacs configuration
+(org-babel-load-file (concat user-emacs-directory "emacs.org"))
 
-;;; Environment fixup
-(use-package exec-path-from-shell
-  :ensure t
-  :if (display-graphic-p))
-(exec-path-from-shell-initialize)
-
-;;; Load my configuration
-(require 'org)
-(require 'ob-tangle)
-
-;; tangle and load the init code if needed
-(let* ((init-org-file (expand-file-name "main.org" user-emacs-directory))
-       (base-name (file-name-sans-extension init-org-file))
-       (exported-file (concat base-name ".el")))
-  (when (file-newer-than-file-p init-org-file exported-file)
-    (org-babel-tangle-file init-org-file exported-file))
-  (require 'bytecomp)
-  (byte-recompile-file exported-file nil 0 t)
-  (message "Loaded %s" exported-file))
-
+;; My custom file
 (setq custom-file (expand-file-name ".custom.el" user-emacs-directory))
 (byte-recompile-file custom-file nil 0 t)
 (message "Loaded %s" custom-file)
+
+;; Garbage collector - decrease threshold to 5 MB
+(add-hook 'after-init-hook (lambda () (setq gc-cons-threshold (* 5 1024 1024))))
+;;; init.el ends here
