0514125b2e4fb155f7ebb50d66e900afee56d832
Marco Dalla Stella <mdallastella@users.noreply.github.com>
2021-02-12 11:23:35 +0100

    Straight (#1)
    * Move to straight.el
    
    * Move to straight.el and some cleanup
    
    * Load custom.el at init
    
    * Selectrum, Consult, Embark and Marginalia
    
    * Back to Ivy, Counsel and Swiper, minor changes
    
    * Load encrypted secret file.
    
    * Back to Selectrum
    
    * Add encrypted secrets.

--- init.el
+++ init.el
@@ -13,39 +13,32 @@
 ;; Increase the garbage collection threshold to 500 MB to ease startup
 (setq gc-cons-threshold (* 500 1024 1024))
 
-;; List package archives and initialize them
-(require 'package)
-(setq package-enable-at-startup nil)
-(setq package-archives
-      ;; Package archives, the usual suspects
-      '(("GNU ELPA" . "https://elpa.gnu.org/packages/")
-        ("MELPA"    . "https://melpa.org/packages/")
-        ("org"      . "http://orgmode.org/elpa/")))
+;; Initialize straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; Replace use-package with straight-use-package
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
+
 (add-to-list 'load-path "~/.emacs.d/lisp/")
-(package-initialize)
 
 (setq load-prefer-newer t)               ; Always load newer compiled files
 (setq ad-redefinition-action 'accept)    ; Silence advice redefinition warnings
 (setq message-log-max 10000)             ; Debugging
 
-;; Bootstrap use-package and dash
-(unless (and (package-installed-p 'use-package)
-             (package-installed-p 'dash)
-             (package-installed-p 'no-littering))
-  (package-refresh-contents)
-  (package-install 'use-package)
-  (package-install 'dash)
-  (package-install 'no-littering))
-
 ;; I need org-mode
-(unless (package-installed-p 'org)
-  (package-refresh-contents)
-  (package-install 'org))
-
-;; I need org-plus-contrib too
-(unless (package-installed-p 'org-plus-contrib)
-  (package-refresh-contents)
-  (package-install 'org-plus-contrib))
+(straight-use-package 'org)
 
 ;; Keep .emacs.d clean
 (use-package no-littering
@@ -65,17 +58,20 @@
         auto-save-file-name-transforms
         `((".*" ,(no-littering-expand-var-file-name "auto-save/") t))))
 
-(use-package gnu-elpa-keyring-update
-  :ensure t)
+;; My custom file
+(setq custom-file (no-littering-expand-etc-file-name "custom.el"))
+
+;; My secrets
+(let ((secret.el (expand-file-name ".secret.el.gpg" user-emacs-directory)))
+  (when (file-exists-p secret.el)
+    (load-library secret.el)))
 
 ;; Load emacs.org - my Emacs configuration
 (org-babel-load-file (expand-file-name "emacs.org" user-emacs-directory))
-
-;; My custom file
-(setq custom-file (expand-file-name ".custom.el" user-emacs-directory))
-(byte-recompile-file custom-file nil 0 t)
+(load custom-file)
 (message "Loaded %s" custom-file)
 
 ;; Garbage collector - decrease threshold to 5 MB
 (add-hook 'after-init-hook (lambda () (setq gc-cons-threshold (* 5 1024 1024))))
 ;;; init.el ends here
+
